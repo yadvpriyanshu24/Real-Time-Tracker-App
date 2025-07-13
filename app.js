2504d98ba179c2a37306f01177f07dc005362ed0
Priyanshu Yadav <yadavpriyanshuyadav7@gmail.com>
2025-07-13 20:48:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express =require("express")
+const app=express();
+const path=require("path");
+
+const http =require("http");
+const  socketio =require("socket.io");
+
+const server= http.createServer(app);
+
+const io= socketio(server);
+
+app.set("view engine", "ejs"); // lowercase and no space before 'ejs'
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+io.on("connection" , function (socket){
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    })
+});
+
+app.get("/", (req, res) => {
+    res.render("index"); // Will render views/index.ejs
+});
+
+server.listen(3000);
